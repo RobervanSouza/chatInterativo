82e74034067c03473396709b53765a5f298aac77
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-05-25 17:11:33 -0300

    ok

--- meu-chat-app/src/components/chats/chats.js
+++ meu-chat-app/src/components/chats/chats.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../Services/firebase";
 import * as S from "./styled";
@@ -7,21 +7,17 @@ import ChatsItem from "../ChatsIte/chatItem";
 
 const Chats = ({ setUserChat, userChat }) => {
   const [ user ] = useAuthState(auth);
-  const chat = db
-    .collection("chats")
-    .where("users", "array-contains", user.email);
+  const chat = db.collection("chats").where("users", "array-contains", user.email);
   const [ emailLogado ] = useCollection(chat);
 
   return (
     <S.Container>
       {emailLogado?.docs.map((item, index) => {
         const users = item.data().users;
-        const emailCadastrado = users.find((email) => email !== user.email);
-        const name = item.data().names;
-        const nome = name.find((email) => email !== user.email);
-        console.log(name, "aqui esta o array de nomes")
-       
-      
+        const emailCadastrado = users.find((email) => email !== user.email)?.email;
+        const names = item.data().users.map(user => user.name);
+        const nome = names.find((name) => name !== user.displayName);
+        console.log(names, "Aqui está a matriz de nomes");
 
         return (
           <S.Chats key={index}>
@@ -32,6 +28,7 @@ const Chats = ({ setUserChat, userChat }) => {
               setUserChat={setUserChat}
               active={userChat?.chatId === item.id ? "active" : ""}
               userName={emailCadastrado} // Passar o email cadastrado como prop
+              userNome={nome} // Passar o nome do usuário como prop
             />
             <S.Chatsdiv />
           </S.Chats>
